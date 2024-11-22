df12ceeaf1da9daa99a4ae0202359d72e99614ad
Nishi <nishi@nishi.boats>
2024-11-22 23:26:57 +0000

    multiboot
    git-svn-id: file:///raid/svn-personal/os-rsmp/trunk@10 2992c0cf-9cd9-9f48-8e2b-7b0614fbb230

--- arch/i386/multiboot.s
+++ arch/i386/multiboot.s
@@ -1,1 +1,36 @@
 ; $Id$
+
+.set MAGIC_NUMBER,	0x1badb002
+.set FLAGS,		(1 << 0) | (1 << 1)
+.set CHECKSUM,		-(MAGIC_NUMBER + FLAGS)
+
+.section .multiboot
+	.align 4
+	.long MAGIC_NUMBER
+	.long FLAGS
+	.long CHECKSUM
+
+.section .data
+	seax: .long 0
+	sebx: .long 0
+
+.section bss
+	.align 16
+	stack_bottom:
+	.skip 16384
+	stack_top:
+
+.section text
+.global _start
+.type _start, @function
+.extern init_kernel
+_start:
+	mov %eax, seax
+	mov %ebx, sebx
+	mov $stack_top, %esp
+	mov seax, %eax
+	mov sebx, %ebx
+	push %ebx
+	push %eax
+	call init_kernel
+.end:
