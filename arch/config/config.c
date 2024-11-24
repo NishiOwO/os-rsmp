4704998e4b8d43b2e0b91e24d0000197c245ed68
Nishi <nishi@nishi.boats>
2024-11-24 09:21:35 +0000

    a
    git-svn-id: file:///raid/svn-personal/os-rsmp/trunk@35 2992c0cf-9cd9-9f48-8e2b-7b0614fbb230

--- arch/config/config.c
+++ arch/config/config.c
@@ -143,8 +143,8 @@ int main(int argc, char** argv){
 	fprintf(f, ".PHONY: all clean arch ../kern ../c\n");
 	fprintf(f, ".SUFFIXES: .c .s .o\n");
 	fprintf(f, "all: osrsmp.iso\n");
-	fprintf(f, "osrsmp.iso: rootfs/kernel ../contrib/boot.cfg\n");
-	fprintf(f, "	cp ../contrib/* ./rootfs/\n");
+	fprintf(f, "osrsmp.iso: rootfs/kernel ../contrib/boot/boot.cfg\n");
+	fprintf(f, "	cp ../contrib/boot/* ./rootfs/\n");
 	fprintf(f, "	mkisofs -R -o $@ -uid 0 -gid 0 -no-emul-boot -b cdboot rootfs\n");
 	fprintf(f, "rootfs/kernel: arch ../kern ../c linker.ld %s drivers.o\n", drivers_o == NULL ? "" : drivers_o);
 	fprintf(f, "	$(LD) -Tlinker.ld $(LDFLAGS) -o $@ arch/*.o kern/*.o %s drivers.o c/libc.a\n", drivers_o == NULL ? "" : drivers_o);
